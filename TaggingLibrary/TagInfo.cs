38c99c5e4ed06350e1f78fdacd9145100755c2fe
John Gietzen <john@gietzen.us>
2020-12-26 19:25:41 -0800

    Adjusted namespace for existing code.

--- TaggingLibrary/TagInfo.cs
+++ TaggingLibrary/TagInfo.cs
@@ -1,6 +1,6 @@
 // Copyright © John Gietzen. All Rights Reserved. This source is subject to the MIT license. Please see license.md for more information.
 
-namespace MediaLibrary.Tagging
+namespace TaggingLibrary
 {
     using System.Collections.Immutable;
 
